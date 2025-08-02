c66bfd2a34585b90b4788da95c9bb538feee6012
Prabhu <vprabhu1771@gmail.com>
2025-08-02 18:20:51 +0530

    Create User.dart

---
+++ lib/models/User.dart
@@ -0,0 +1,17 @@
+class User{
+
+  String name;
+  String email;
+  String avatar;
+
+  User({
+    required this.name,
+    required this.email,
+    required this.avatar
+  });
+
+  User.fromJson(Map<String, dynamic>json):
+    name=json['name'],
+    email=json['email'],
+    avatar=json['avatar'];
+}
