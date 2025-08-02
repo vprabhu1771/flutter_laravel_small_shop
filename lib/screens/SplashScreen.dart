33d17f16accfbb6e9310fc2502b213b5e60ee559
Prabhu <vprabhu1771@gmail.com>
2025-08-02 12:05:34 +0530

    Create SplashScreen.dart

---
+++ lib/screens/SplashScreen.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatefulWidget {
+  final String title;
+  const SplashScreen({super.key, required this.title});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text(widget.title),),
+      body: Center(
+        child: Text(widget.title),
+      ),
+    );
+  }
+}
+
